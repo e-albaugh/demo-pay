34bd4ba808479bf5fc5ef2ac78679ee4ff5176f4
Eric Albaugh <eric.albaugh@microfocus.com>
2021-08-05 20:34:16 -0400

    user story #1211 add unit tests for info handling

--- src/test/java/com/mf/presales_pay/PaymentGatewaySmokeTest.java
+++ src/test/java/com/mf/presales_pay/PaymentGatewaySmokeTest.java
@@ -79,3 +79,47 @@ public class PaymentGatewaySmokeTest extends TestCase {
 
 
 }
+
+public class StripeSmokeTest extends TestCase {
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public StripeSmokeTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(StripeSmokeTest.class);
+    }
+ 
+    public void testPaymentIntentsResource() {
+        assertTrue(true);
+    }
+
+    public void testOnSessionPayments() {
+        assertTrue(true);
+    }
+
+    public void testOffSessionPayments() {
+        assertTrue(true);
+    }
+
+    public void testSaveCard() {
+        assertTrue(true);
+    }
+
+    public void testReuseCard() {
+        assertTrue(true);
+    }
+
+    public void testOneTimePayment() {
+        assertTrue(true);
+    }
+    
+}
+
